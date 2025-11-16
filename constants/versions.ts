cf246d43ab4364443cead6ce9e8ef8360c84be03
JoycieC <tajoyciec@googlemail.com>
2025-11-16 10:08:14 +0000

    Add centralized version list and labels for IIDX including 33

---
+++ constants/versions.ts
@@ -0,0 +1,27 @@
+export const SUPPORTED_IIDX_VERSIONS = [
+  14,15,16,17,18,19,20,
+  21,22,23,24,25,26,27,28,29,30,31,32,33
+];
+
+export const IIDX_VERSION_NAMES: Record<number, string> = {
+  14: "beatmaniaIIDX 14 GOLD",
+  15: "beatmaniaIIDX 15 DJ TROOPERS",
+  16: "beatmaniaIIDX 16 EMPRESS",
+  17: "beatmaniaIIDX 17 SIRIUS",
+  18: "beatmaniaIIDX 18 Resort Anthem",
+  19: "beatmaniaIIDX 19 Lincle",
+  20: "beatmaniaIIDX 20 tricoro",
+  21: "beatmaniaIIDX 21 SPADA",
+  22: "beatmaniaIIDX 22 PENDUAL",
+  23: "beatmaniaIIDX 23 copula",
+  24: "beatmaniaIIDX 24 SINOBUZ",
+  25: "beatmaniaIIDX 25 CANNON BALLERS",
+  26: "beatmaniaIIDX 26 Rootage",
+  27: "beatmaniaIIDX 27 HEROIC VERSE",
+  28: "beatmaniaIIDX 28 BISTROVER",
+  29: "beatmaniaIIDX 29 CastHour",
+  30: "beatmaniaIIDX 30 RESIDENT",
+  31: "beatmaniaIIDX 31 EPOLIS",
+  32: "beatmaniaIIDX 32 Pinky Crush",
+  33: "beatmaniaIIDX 33 Sparkle Shower",
+};
